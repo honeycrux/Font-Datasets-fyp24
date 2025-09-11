5384fe1415df6d6220a2c36731cbca8167e7fd01
honeycrux <114286060+honeycrux@users.noreply.github.com>
2025-09-11 21:35:11 +0800

    update scripts

--- scripts/fyp23/prepare-fyp23-dataset.sh
+++ scripts/fyp23/prepare-fyp23-dataset.sh
@@ -7,7 +7,10 @@ mkdir fyp23-dataset
 chmod 777 fyp23-dataset
 
 # Download the dataset
-# Coming soon
+wget "https://github.com/honeycrux/Font-Datasets-fyp24/raw/refs/heads/main/releases/fyp23-dataset-source.zip?download=" -O fyp23-dataset-source/fyp23-dataset.zip
+unzip -qq fyp23-dataset-source/fyp23-dataset.zip -d fyp23-dataset-source
+rm -f fyp23-dataset-source/fyp23-dataset.zip
+chmod -R 777 fyp23-dataset-source
 
 # Create content and target images
 python -m scripts.fyp23.step_0_create_content_and_target_images
--- scripts/zhuojg/prepare-zhuojg-dataset.sh
+++ scripts/zhuojg/prepare-zhuojg-dataset.sh
@@ -2,6 +2,10 @@
 mkdir zhuojg-dataset-source
 chmod 777 zhuojg-dataset-source
 
+# Make zhuojg-dataset directory
+mkdir zhuojg-dataset
+chmod -R 777 zhuojg-dataset
+
 # Download the dataset
 # (The following URL should be the second dataset on the README of https://github.com/zhuojg/chinese-calligraphy-dataset)
 wget "https://drive.usercontent.google.com/download?id=10QJrw0Qdk4O1bIrehCLmdiCkwpLbGVe8&export=download&confirm=t" -O zhuojg-dataset-source/zhuojg-dataset.zip
@@ -9,10 +13,6 @@ unzip -qq zhuojg-dataset-source/zhuojg-dataset.zip -d zhuojg-dataset-source
 rm -f zhuojg-dataset-source/zhuojg-dataset.zip
 rm -rf zhuojg-dataset-source/__MACOSX
 chmod -R 777 zhuojg-dataset-source
-
-# Make zhuojg-dataset directory
-mkdir zhuojg-dataset
-chmod -R 777 zhuojg-dataset
 
 # Create target images
 python -m scripts.zhuojg.step_0_create_target_images
